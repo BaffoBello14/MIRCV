4ae08e8e52e1ebf264f22acebb8a0cfe55384e2c
Giulio Bello <giuliobello12@gmail.com>
2023-10-19 19:52:32 +0200

    commentato il codice ed aggiunti main di prova

--- src/main/java/it/unipi/MIRCV/Utils/Preprocessing/Preprocess.java
+++ src/main/java/it/unipi/MIRCV/Utils/Preprocessing/Preprocess.java
@@ -1,16 +1,34 @@
 package it.unipi.MIRCV.Utils.Preprocessing;
+
 import ca.rmen.porterstemmer.PorterStemmer;
 
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.List;
 import java.util.Locale;
 
 public class Preprocess {
-    private static String Path_to_File="prova.tsv";
-    private static final String Path_to_Stopwords="stopWord/stopword.txt";
-    private static final PorterStemmer PStermmer=new PorterStemmer();
+    
+    private static final String PATH_TO_STOPWORDS = "stopWord/stopword.txt";
+    private static final PorterStemmer PORTER_STEMMER = new PorterStemmer();
+    private static List<String> stopwords = new ArrayList<>();
+
+    // Blocco statico per caricare le stopwords all'inizializzazione della classe.
+    static {
+        try (BufferedReader bufferedReader = new BufferedReader(new FileReader(PATH_TO_STOPWORDS))) {
+            String line;
+            while ((line = bufferedReader.readLine()) != null) {
+                stopwords.add(line);
+            }
+        } catch (IOException e) {
+            System.out.println("Problemi nell'apertura del file delle stopwords");
+            e.printStackTrace();
+        }
+    }
+
+    // Espressioni regolari per la pulizia del testo.
     private static final String URL_REGEX = "[(http(s)?):\\/\\/(www\\.)?a-zA-Z0-9@:%._\\+~#=]{2,256}\\.[a-z]{2,6}\\b([-a-zA-Z0-9@:%_\\+.~#?&//=]*)";
     private static final String HTML_TAGS_REGEX = "<[^>]+>";
     private static final String NON_LETTER_REGEX = "[^a-zA-Z ]";
@@ -18,68 +36,94 @@ public class Preprocess {
     private static final String CONSECUTIVE_LETTERS_REGEX = "(.)\\1{2,}";
     private static final String CAMEL_CASE_REGEX = "(?<=[a-z])(?=[A-Z])";
 
-    private static ArrayList<String> stopwords=new ArrayList<>();
-    public static String [] parserLineCollection(String line){
+    /**
+     * Funzione per dividere le linee del file TSV in colonne.
+     */
+    public static String[] parseLine(String line) {
         return line.split("\t");
     }
-    public static String textCleaning(String text){
-        // Remove URLs
-        String textWithoutURLs = text.replaceAll(URL_REGEX, " ");
 
-        // Remove HTML tags
-        String textWithoutHTMLTags = textWithoutURLs.replaceAll(HTML_TAGS_REGEX, " ");
-
-        // Remove non-letter characters
-        String textWithoutNonLetters = textWithoutHTMLTags.replaceAll(NON_LETTER_REGEX, " ");
-
-        // Remove sequential spaces
-        String textWithoutMultipleSpaces = textWithoutNonLetters.replaceAll(MULTIPLE_SPACES_REGEX, " ");
-
-        // Remove consecutive letters and spaces at begin and end
-        return textWithoutMultipleSpaces.replaceAll(CONSECUTIVE_LETTERS_REGEX, "$1$1").trim();
-
+    /**
+     * Funzione per pulire il testo: rimuove URL, tag HTML, caratteri non letterali, 
+     * lettere consecutive e spazi multipli.
+     */
+    public static String cleanText(String text) {
+        return text.replaceAll(URL_REGEX, " ")
+                   .replaceAll(HTML_TAGS_REGEX, " ")
+                   .replaceAll(NON_LETTER_REGEX, " ")
+                   .replaceAll(MULTIPLE_SPACES_REGEX, " ")
+                   .replaceAll(CONSECUTIVE_LETTERS_REGEX, "$1$1")
+                   .trim();
     }
 
-    public static String [] tokenizer(String text){
-        ArrayList<String> finalTokens=new ArrayList<>();
-        String[] tokens=text.split(" ");
-        for(String word: tokens){
-            String [] withoutCamelCase=word.split(CAMEL_CASE_REGEX);
-            for(String subToken:withoutCamelCase){
-                finalTokens.add(subToken.toLowerCase(Locale.ROOT));
+    /**
+     * Funzione di tokenizzazione: divide il testo in parole e gestisce le parole in camelCase.
+     */
+    public static List<String> tokenize(String text) {
+        List<String> tokens = new ArrayList<>();
+        String[] words = text.split(" ");
+        for (String word : words) {
+            String[] withoutCamelCase = word.split(CAMEL_CASE_REGEX);
+            for (String token : withoutCamelCase) {
+                tokens.add(token.toLowerCase(Locale.ROOT));
             }
         }
-        return finalTokens.toArray(new String[finalTokens.size()]);
+        return tokens;
     }
 
-    public static String[] stemmer(String [] tokens){
-        if(tokens.length==0)
-            return null;
-        ArrayList<String> stemmedTokens=new ArrayList<>();
-        for(String word:tokens){
-            stemmedTokens.add(PStermmer.stemWord(word));
+    /**
+     * Rimuove le stopwords dalle parole.
+     */
+    public static List<String> removeStopwords(List<String> tokens) {
+        List<String> filteredTokens = new ArrayList<>();
+        for (String token : tokens) {
+            if (!stopwords.contains(token)) {
+                filteredTokens.add(token);
+            }
         }
-        return stemmedTokens.toArray(new String[stemmedTokens.size()]);
+        return filteredTokens;
     }
-    public static String[] stopwordRemoval(String [] tokens){
-        ArrayList<String> tokensWithouStopwords=new ArrayList<>();
-        if(stopwords.isEmpty()){
-            try(BufferedReader bufferedReader=new BufferedReader(new FileReader(Path_to_Stopwords))){
-                String line;
-                while((line=bufferedReader.readLine())!=null){
-                    stopwords.add(line);
+
+    /**
+     * Applica la stemming alle parole utilizzando l'algoritmo Porter.
+     */
+    public static List<String> applyStemming(List<String> tokens) {
+        List<String> stemmedTokens = new ArrayList<>();
+        for (String token : tokens) {
+            stemmedTokens.add(PORTER_STEMMER.stemWord(token));
+        }
+        return stemmedTokens;
+    }
+
+    /**
+     * Funzione di test per processare un file TSV e stampare i risultati.
+     */
+    public static void main(String[] args) {
+        String tsvFilePath = "prova.tsv";
+        int columnIndexToProcess = 1;
+    
+        try (BufferedReader reader = new BufferedReader(new FileReader(tsvFilePath))) {
+            String line;
+            while ((line = reader.readLine()) != null) {
+                String[] columns = parseLine(line);
+                
+                if (columns.length > columnIndexToProcess) {
+                    String text = columns[columnIndexToProcess];
+                    String cleanedText = cleanText(text);
+                    List<String> tokens = tokenize(cleanedText);
+                    List<String> tokensWithoutStopwords = removeStopwords(tokens);
+                    List<String> stemmedTokens = applyStemming(tokensWithoutStopwords);
+
+                    System.out.println("Testo originale: " + text);
+                    System.out.println("Testo pulito: " + cleanedText);
+                    System.out.println("Token: " + String.join(", ", tokens));
+                    System.out.println("Token senza stopwords: " + String.join(", ", tokensWithoutStopwords));
+                    System.out.println("Token con stemming: " + String.join(", ", stemmedTokens));
+                    System.out.println("---------------------------------------------");
                 }
-
-            }catch(IOException e){
-                System.out.println("problems with opening the stopword file");
-                e.printStackTrace();
             }
+        } catch (IOException e) {
+            System.out.println("Errore durante la lettura del file TSV: " + e.getMessage());
         }
-        for(String word :tokens){
-            if(!stopwords.contains(word)){
-                tokensWithouStopwords.add(word);
-            }
-        }
-        return tokensWithouStopwords.toArray(new String[tokensWithouStopwords.size()]);
     }
 }
--- src/main/java/it/unipi/MIRCV/VariableByteEncoder.java
+++ src/main/java/it/unipi/MIRCV/VariableByteEncoder.java
@@ -1,29 +1,88 @@
-package it.unipi.MIRCV.Utils;
+package it.unipi.MIRCV;
 
 public class VariableByteEncoder {
-    public static byte[] encode(int value){
-        int numBytes=(int)Math.ceil((Math.log(value+1)/Math.log(128)));
-        byte [] encodedBytes=new byte[numBytes];
-        for (int i=numBytes-1;i>=0;i--){
-            byte currentByte=(byte)(value%128);
-            value/=128;
-            if(i>0){
-                currentByte|= (byte) 128;
+
+    /**
+     * Codifica un valore intero utilizzando la codifica "Variable Byte".
+     *
+     * @param value L'intero da codificare.
+     * @return Un array di byte che rappresenta il valore in codifica "Variable Byte".
+     */
+    public static byte[] encode(int value) {
+        // Calcola il numero di byte necessari per rappresentare il valore dato.
+        // Ogni byte può rappresentare 7 bit del valore, più 1 bit come segnalatore di continuazione.
+        int numBytes = (int) Math.ceil((Math.log(value + 1) / Math.log(128)));
+        byte[] encodedBytes = new byte[numBytes];
+
+        // Inizia dalla posizione del byte meno significativo e lavora all'indietro.
+        for (int i = numBytes - 1; i >= 0; i--) {
+            // Estrai i 7 bit meno significativi del valore.
+            byte currentByte = (byte) (value % 128);
+            
+            // Riduci il valore, rimuovendo i bit che abbiamo appena estratto.
+            value /= 128;
+            
+            // Se non siamo sull'ultimo byte, imposta il bit più significativo (segnalatore di continuazione) su 1.
+            if (i > 0) {
+                currentByte |= (byte) 128;
             }
-            encodedBytes[i]=currentByte;
+            
+            // Assegna il byte codificato all'array di output nella posizione corrente.
+            encodedBytes[i] = currentByte;
         }
+
         return encodedBytes;
     }
-    public static int decode(byte[] encodedBytes){
-        int decodedValue=0;
-        for (byte b:encodedBytes){
-            int value=b&0x7f;
-            decodedValue=(decodedValue<<7)|value;
-            if((b&0x80)==0){
-                break;
-            }
+
+    /**
+     * Decodifica un valore intero da una rappresentazione "Variable Byte".
+     *
+     * @param encodedBytes L'array di byte in codifica "Variable Byte".
+     * @return L'intero decodificato.
+     */
+    public static int decode(byte[] encodedBytes) {
+        int decodedValue = 0;
+
+        // Per ogni byte nell'array di input...
+        for (byte b : encodedBytes) {
+            // Estrai i 7 bit meno significativi dal byte corrente.
+            int value = b & 0x7f;
+
+            // Aggiungi questi bit al valore decodificato, spostandoli nella posizione corretta.
+            decodedValue = (decodedValue << 7) | value;
         }
+
         return decodedValue;
     }
 
+    public static void main(String[] args) {
+        int[] testValues = {1, 127, 128, 255, 256, 16383, 16384, 2097151};
+    
+        System.out.println("Test NewVariableByteEncoder:");
+    
+        for (int value : testValues) {
+            // Inizia a misurare il tempo per la codifica
+            long startTimeEncode = System.nanoTime();
+            byte[] encoded = VariableByteEncoder.encode(value);
+            long endTimeEncode = System.nanoTime();
+    
+            // Inizia a misurare il tempo per la decodifica
+            long startTimeDecode = System.nanoTime();
+            int decoded = VariableByteEncoder.decode(encoded);
+            long endTimeDecode = System.nanoTime();
+    
+            // Calcola il tempo trascorso per la codifica e la decodifica
+            long durationEncode = endTimeEncode - startTimeEncode;
+            long durationDecode = endTimeDecode - startTimeDecode;
+    
+            System.out.print("Valore originale: " + value + " -> Codificato: ");
+            for (byte b : encoded) {
+                System.out.print(String.format("%8s", Integer.toBinaryString(b & 0xFF)).replace(' ', '0') + " ");
+            }
+            System.out.println("-> Decodificato: " + decoded);
+            System.out.println("Tempo di codifica: " + durationEncode + " nanosecondi");
+            System.out.println("Tempo di decodifica: " + durationDecode + " nanosecondi");
+            System.out.println("-------------------------------------------------");
+        }
+    }
 }
