8efb6ea71433ced967dea9bd9a4e2608cd00de9d
LiuChang01 <c.liu4@studenti.unipi.it>
2023-10-23 15:00:51 +0200

    write in main to try the docIndex class

--- src/main/java/it/unipi/MIRCV/Main.java
+++ src/main/java/it/unipi/MIRCV/Main.java
@@ -32,30 +32,36 @@ public class Main {
 
 package it.unipi.MIRCV;
 
+import it.unipi.MIRCV.Utils.Indexing.DocIndex;
+import it.unipi.MIRCV.Utils.Indexing.DocIndexEntry;
+import it.unipi.MIRCV.Utils.Preprocessing.Preprocess;
+
 import java.io.*;
+import java.util.List;
+import java.util.Map;
 
 public class Main {
     public static void main(String[] args) throws Exception {
 
-        // Scrivi nel file come dati binari
-        try (DataOutputStream dataOutputStream = new DataOutputStream(new FileOutputStream("data.dat"))) {
-            dataOutputStream.write("ciao".getBytes("UTF-8"));
-            dataOutputStream.writeChar('\t');
-            dataOutputStream.writeInt(10);
+        DocIndex docIndex=new DocIndex();
+        BufferedReader reader=new BufferedReader(new FileReader("prova.tsv"));
+        String line;
+        String [] docno;
+        while((line= reader.readLine())!=null){
+            docno= Preprocess.parseLine(line);
+            System.out.println(docno[0]);
+            line=Preprocess.cleanText(docno[1]);
+            List<String>tokens=Preprocess.tokenize(line);
+            tokens=Preprocess.removeStopwords(tokens);
+            List<String>tokenized=Preprocess.applyStemming(tokens);
+            for(String word :tokenized){
+                System.out.print(word+" ");
+            }
+            System.out.println();
+            docIndex.addDocument(Integer.parseInt(docno[0]),Integer.parseInt(docno[0]),tokenized.size());
         }
-
-        // Leggi dal file come dati binari
-        try (RandomAccessFile randomAccessFile = new RandomAccessFile("data.dat", "r");
-             DataInputStream dataInputStream = new DataInputStream(new FileInputStream(randomAccessFile.getFD()))) {
-
-            byte[] readBytes = new byte[4]; // assumiamo che "ciao" sia sempre di 4 byte
-            dataInputStream.read(readBytes);
-            String readString = new String(readBytes, "UTF-8");
-
-            char tabChar = dataInputStream.readChar();  // legge il carattere tabulatore
-            int number = dataInputStream.readInt();
-
-            System.out.print(readString + tabChar + number);
+        for (Map.Entry<Integer, DocIndexEntry> entry : docIndex.getDocumentIndex().entrySet() ){
+            System.out.println("doc_id"+entry.getKey()+" doc_no|len "+entry.getValue());
         }
     }
 }
