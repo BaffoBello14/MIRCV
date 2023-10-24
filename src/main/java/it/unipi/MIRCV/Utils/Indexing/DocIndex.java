702107847bcd33a3a4e5dbcb6b6a2a9049590a47
LiuChang01 <c.liu4@studenti.unipi.it>
2023-10-24 11:57:06 +0200

    develop of write to disk method of doc index

--- src/main/java/it/unipi/MIRCV/Utils/Indexing/DocIndex.java
+++ src/main/java/it/unipi/MIRCV/Utils/Indexing/DocIndex.java
@@ -1,11 +1,18 @@
 package it.unipi.MIRCV.Utils.Indexing;
 
+import java.io.File;
+import java.io.IOException;
+import java.nio.MappedByteBuffer;
+import java.nio.channels.FileChannel;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.HashMap;
+import java.util.List;
 
 public class DocIndex {
     private HashMap<Integer,DocIndexEntry> documentIndex;
+    private static int offset=0;
+    private static final int ENTRY_SIZE=4+4+8;
     public DocIndex(){
         documentIndex=new HashMap<>();
     }
@@ -24,5 +31,28 @@ public class DocIndex {
         ArrayList<Integer>sortedDocIndex=new ArrayList<>(documentIndex.keySet());
         Collections.sort(sortedDocIndex);
         return sortedDocIndex;
+    }
+    public boolean write2Disk(FileChannel fileChannel){
+        if(fileChannel==null){
+            return false;
+        }
+        try{
+            MappedByteBuffer mappedByteBuffer= fileChannel.map(FileChannel.MapMode.READ_WRITE,offset, (long) ENTRY_SIZE * documentIndex.size());
+            if(mappedByteBuffer==null){
+                return false;
+            }
+            List<Integer> doc_ids=sortDocIndex();
+            for(int i:doc_ids){
+                mappedByteBuffer.putInt(i);
+                mappedByteBuffer.putInt(documentIndex.get(i).getDoc_no());
+                mappedByteBuffer.putLong(documentIndex.get(i).getDoc_size());
+            }
+            return true;
+
+        }catch(IOException e){
+            System.out.println("problem with file channel at writing doc index to file");
+            e.printStackTrace();
+            return false;
+        }
     }
 }
