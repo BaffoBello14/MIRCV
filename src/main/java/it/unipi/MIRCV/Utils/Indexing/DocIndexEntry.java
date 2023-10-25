8ce2ce10eb7d933bb78d76e7b61759864a8331ff
LiuChang01 <c.liu4@studenti.unipi.it>
2023-10-25 14:49:06 +0200

    Update DocIndexEntry.java

--- src/main/java/it/unipi/MIRCV/Utils/Indexing/DocIndexEntry.java
+++ src/main/java/it/unipi/MIRCV/Utils/Indexing/DocIndexEntry.java
@@ -1,31 +1,44 @@
 package it.unipi.MIRCV.Utils.Indexing;
 
+import java.io.IOException;
+import java.nio.MappedByteBuffer;
+import java.nio.channels.FileChannel;
+import java.nio.charset.StandardCharsets;
+
 public class DocIndexEntry {
-    private int doc_no;
+    private String doc_no;
     private long doc_size;
-    public DocIndexEntry(int doc_no,long doc_size){
-        this.doc_no=doc_no;
+    protected static  final int DOC_NO_LENGTH=8;
+    protected static final int DOC_INDEX_ENTRY_SIZE=DOC_NO_LENGTH+8+4;
+    public DocIndexEntry(String doc_no,long doc_size){
+        this.doc_no=padNumberWithZeros(doc_no,DOC_NO_LENGTH);
         this.doc_size=doc_size;
     }
-
-    public int getDoc_no() {
+    public long write2Disk(FileChannel fileChannel,long offset,int doc_id){
+        try {
+            MappedByteBuffer mappedByteBuffer=fileChannel.map(FileChannel.MapMode.READ_WRITE,offset,DOC_INDEX_ENTRY_SIZE);
+            mappedByteBuffer.putInt(doc_id);
+            mappedByteBuffer.put(padNumberWithZeros(doc_no,DOC_NO_LENGTH).getBytes(StandardCharsets.UTF_8));
+            mappedByteBuffer.putLong(doc_size);
+            return offset+DOC_INDEX_ENTRY_SIZE;
+        }catch (IOException e){
+            System.out.println("problems with the writing to disk of doc index ");
+            e.printStackTrace();
+            return -1;
+        }
+    }
+    public String getDoc_no() {
         return doc_no;
     }
-
-    public void setDoc_no(int doc_no) {
-        this.doc_no = doc_no;
-    }
-
     public long getDoc_size() {
         return doc_size;
     }
 
-    public void setDoc_size(long doc_size) {
-        this.doc_size = doc_size;
-    }
     @Override
     public String toString(){
         return doc_no+" "+doc_size;
     }
-
+    public static String padNumberWithZeros(String numberString, int totalWidth) {
+        return String.format("%0" + totalWidth + "d", Long.parseLong(numberString));
+    }
 }
