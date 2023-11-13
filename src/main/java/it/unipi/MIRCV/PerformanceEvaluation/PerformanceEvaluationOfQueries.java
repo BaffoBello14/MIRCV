8b9cbbcd14e6b25c63536b965e0a6d269448336e
Giulio Bello <giuliobello12@gmail.com>
2023-11-13 10:58:30 +0100

    change of pruning class and some comment

--- src/main/java/it/unipi/MIRCV/PerformanceEvaluation/PerformanceEvaluationOfQueries.java
+++ src/main/java/it/unipi/MIRCV/PerformanceEvaluation/PerformanceEvaluationOfQueries.java
@@ -13,9 +13,17 @@ import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.zip.GZIPInputStream;
+
+/**
+ * The PerformanceEvaluationOfQueries class performs the performance evaluation of search engine queries.
+ */
 public class PerformanceEvaluationOfQueries {
     private static String PATH_TO_ARCHIVE_COLLECTION = "./Collection/msmarco-test2019-queries.tsv.gz";
-    public static void execute(){
+
+    /**
+     * Executes the performance evaluation of search engine queries.
+     */
+    public static void execute() {
         CollectionStatistics.readFromDisk();
         PathAndFlags.readFlagsFromDisk();
         String[] lineofDoc;
@@ -26,10 +34,10 @@ public class PerformanceEvaluationOfQueries {
             InputStream gzip = new GZIPInputStream(file);
             BufferedReader reader = new BufferedReader(new InputStreamReader(gzip, StandardCharsets.UTF_8));
             String line;
-            BufferedWriter bufferedWriterDAATTFIDF=new BufferedWriter(new FileWriter("./PerformanceEvaluatedFile/DAATTFIDF.txt"));
-            BufferedWriter bufferedWriterDAATBM25=new BufferedWriter(new FileWriter("./PerformanceEvaluatedFile/DAATBM25.txt"));
-            BufferedWriter bufferedWriterDYNAMICPRUNINGTFIDF=new BufferedWriter(new FileWriter("./PerformanceEvaluatedFile/DYNAMICPRUNINGTFIDF.txt"));
-            BufferedWriter bufferedWriterDYNAMICPRUNINGBM25=new BufferedWriter(new FileWriter("./PerformanceEvaluatedFile/DYNAMICPRUNINGBM25.txt"));
+            BufferedWriter bufferedWriterDAATTFIDF = new BufferedWriter(new FileWriter("./PerformanceEvaluatedFile/DAATTFIDF.txt"));
+            BufferedWriter bufferedWriterDAATBM25 = new BufferedWriter(new FileWriter("./PerformanceEvaluatedFile/DAATBM25.txt"));
+            BufferedWriter bufferedWriterDYNAMICPRUNINGTFIDF = new BufferedWriter(new FileWriter("./PerformanceEvaluatedFile/DYNAMICPRUNINGTFIDF.txt"));
+            BufferedWriter bufferedWriterDYNAMICPRUNINGBM25 = new BufferedWriter(new FileWriter("./PerformanceEvaluatedFile/DYNAMICPRUNINGBM25.txt"));
 
             while ((line = reader.readLine()) != null) {
                 if (line.trim().isEmpty()) {
@@ -40,30 +48,31 @@ public class PerformanceEvaluationOfQueries {
                     continue;
                 }
 
-                qno=lineofDoc[0];
-                ArrayList<Integer>answerOfSearchEngine= Processer.processQuery(lineofDoc[1],10,false,"tfidf");
-                write2File(bufferedWriterDAATTFIDF,answerOfSearchEngine,qno);
-                answerOfSearchEngine=Processer.processQuery(lineofDoc[1],10,false,"bm25");
-                write2File(bufferedWriterDAATBM25,answerOfSearchEngine,qno);
-                PathAndFlags.DYNAMIC_PRUNING=true;
-                answerOfSearchEngine= Processer.processQuery(lineofDoc[1],10,false,"tfidf");
-                write2File(bufferedWriterDYNAMICPRUNINGTFIDF,answerOfSearchEngine,qno);
-                answerOfSearchEngine=Processer.processQuery(lineofDoc[1],10,false,"bm25");
-                write2File(bufferedWriterDYNAMICPRUNINGBM25,answerOfSearchEngine,qno);
+                qno = lineofDoc[0];
+                ArrayList<Integer> answerOfSearchEngine = Processer.processQuery(lineofDoc[1], 10, false, "tfidf");
+                write2File(bufferedWriterDAATTFIDF, answerOfSearchEngine, qno);
+                answerOfSearchEngine = Processer.processQuery(lineofDoc[1], 10, false, "bm25");
+                write2File(bufferedWriterDAATBM25, answerOfSearchEngine, qno);
+                PathAndFlags.DYNAMIC_PRUNING = true;
+                answerOfSearchEngine = Processer.processQuery(lineofDoc[1], 10, false, "tfidf");
+                write2File(bufferedWriterDYNAMICPRUNINGTFIDF, answerOfSearchEngine, qno);
+                answerOfSearchEngine = Processer.processQuery(lineofDoc[1], 10, false, "bm25");
+                write2File(bufferedWriterDYNAMICPRUNINGBM25, answerOfSearchEngine, qno);
             }
             bufferedWriterDAATBM25.close();
             bufferedWriterDAATTFIDF.close();
             bufferedWriterDYNAMICPRUNINGBM25.close();
             bufferedWriterDYNAMICPRUNINGTFIDF.close();
 
-        }catch (IOException e){
+        } catch (IOException e) {
             e.printStackTrace();
             System.out.println("Problems with opening the query file to perform a performance evaluation");
 
         }
     }
-    private static void write2File(BufferedWriter bufferedWriter,ArrayList<Integer>answerOfSearchEngine,String qno){
-        try{
+
+    private static void write2File(BufferedWriter bufferedWriter, ArrayList<Integer> answerOfSearchEngine, String qno) {
+        try {
             if (answerOfSearchEngine == null) {
                 bufferedWriter.write(qno + " Q0 null\n");
             } else {
@@ -71,7 +80,7 @@ public class PerformanceEvaluationOfQueries {
                     bufferedWriter.write(qno + " Q0 " + DocIndex.getInstance().getDoc_NO(i) + "\n");
                 }
             }
-        }catch (IOException e){
+        } catch (IOException e) {
             e.printStackTrace();
             System.out.println("Problems with opening the query file to perform a performance evaluation in the write method");
         }
@@ -79,6 +88,5 @@ public class PerformanceEvaluationOfQueries {
 
     public static void main(String[] args) {
         execute();
-
     }
 }
--- src/main/java/it/unipi/MIRCV/Query/MaxScoreDynamicPruning.java
+++ src/main/java/it/unipi/MIRCV/Query/MaxScoreDynamicPruning.java
@@ -8,167 +8,256 @@ import java.util.ArrayList;
 
 import java.util.Comparator;
 
+/**
+ * The MaxScoreDynamicPruning class performs dynamic pruning during query processing using the max score algorithm.
+ */
 public class MaxScoreDynamicPruning {
-    private static void sortByUpperbound(ArrayList<PostingIndex> postingIndices){
+
+    /**
+     * Sorts the posting indices by their upper bounds.
+     *
+     * @param postingIndices The posting indices to be sorted.
+     */
+    private static void sortByUpperbound(ArrayList<PostingIndex> postingIndices) {
         postingIndices.sort(Comparator.comparing(PostingIndex::getUpperBound));
     }
-    public static TopKPriorityQueue<Pair<Float,Integer>>maxScore(ArrayList<PostingIndex>postings,int k, String TFIDFOrBM25, boolean conjunctive){
+
+    /**
+     * Performs the max score dynamic pruning algorithm for a set of postings.
+     *
+     * @param postings      The list of posting indices.
+     * @param k             The number of top documents to retrieve.
+     * @param TFIDFOrBM25   The scoring model to use (TFIDF or BM25).
+     * @param conjunctive   Whether to use conjunctive retrieval.
+     * @return A priority queue containing the top-k documents with their scores.
+     */
+    public static TopKPriorityQueue<Pair<Float, Integer>> maxScore(ArrayList<PostingIndex> postings, int k, String TFIDFOrBM25, boolean conjunctive) {
         sortByUpperbound(postings);
+
         for (PostingIndex index : postings) {
             index.openList();
             index.next();
         }
-        TopKPriorityQueue<Pair<Float,Integer>>topKPriorityQueue= new TopKPriorityQueue<>(k,Comparator.comparing(Pair::getKey));
-        float[]ub=new float[postings.size()];
-        ub[0]=postings.get(0).getUpperBound();
-        for(int i=1;i< postings.size();i++){
-            ub[i]=ub[i-1]+postings.get(i).getUpperBound();
+
+        TopKPriorityQueue<Pair<Float, Integer>> topKPriorityQueue = new TopKPriorityQueue<>(k, Comparator.comparing(Pair::getKey));
+        float[] ub = new float[postings.size()];
+        ub[0] = postings.get(0).getUpperBound();
+
+        for (int i = 1; i < postings.size(); i++) {
+            ub[i] = ub[i - 1] + postings.get(i).getUpperBound();
         }
-        float threshold=0.0F;
-        int pivot=0;
+
+        float threshold = 0.0F;
+        int pivot = 0;
         int current;
         boolean skip;
-        while (pivot< postings.size()&&!isFinished(postings,pivot, postings.size())){
-            float score=0.0F;
-            skip=false;
-            current=conjunctive?get_doc_id(postings,pivot, postings.size()):getMinDocId(postings,pivot,postings.size());
-            if(current==0){
+
+        while (pivot < postings.size() && !isFinished(postings, pivot, postings.size())) {
+            float score = 0.0F;
+            skip = false;
+            current = conjunctive ? get_doc_id(postings, pivot, postings.size()) : getMinDocId(postings, pivot, postings.size());
+
+            if (current == 0) {
                 break;
             }
-            for(int i=pivot;i<postings.size();i++){
-                if(postings.get(i).getPostingActual()!=null&&postings.get(i).getPostingActual().getDoc_id()==current){
-                    score+=Scorer.score(postings.get(i).getPostingActual(),postings.get(i).getIdf(),TFIDFOrBM25);
+
+            for (int i = pivot; i < postings.size(); i++) {
+                if (postings.get(i).getPostingActual() != null && postings.get(i).getPostingActual().getDoc_id() == current) {
+                    score += Scorer.score(postings.get(i).getPostingActual(), postings.get(i).getIdf(), TFIDFOrBM25);
                     postings.get(i).next();
-                } else if (conjunctive&&postings.get(i).getPostingActual()==null) {
+                } else if (conjunctive && postings.get(i).getPostingActual() == null) {
                     return topKPriorityQueue;
                 }
-
             }
-            for(int i=pivot-1;i>=0;i--){
-                if(score+ub[i]<threshold){
-                    skip=true;
+
+            for (int i = pivot - 1; i >= 0; i--) {
+                if (score + ub[i] < threshold) {
+                    skip = true;
                     break;
                 }
 
-                if(postings.get(i).getPostingActual()!=null){
-                    if(postings.get(i).getPostingActual().getDoc_id()<current){
-                        Posting geq=postings.get(i).nextGEQ(current);
-                        if(geq==null&&conjunctive||conjunctive&&geq.getDoc_id()!=current){
-                            skip=true;
+                if (postings.get(i).getPostingActual() != null) {
+                    if (postings.get(i).getPostingActual().getDoc_id() < current) {
+                        Posting geq = postings.get(i).nextGEQ(current);
+
+                        if (geq == null && conjunctive || (conjunctive && geq.getDoc_id() != current)) {
+                            skip = true;
                             break;
                         }
-                        if(geq!=null&&geq.getDoc_id()==current){
-                            score+=Scorer.score(geq,postings.get(i).getIdf(),TFIDFOrBM25);
+
+                        if (geq != null && geq.getDoc_id() == current) {
+                            score += Scorer.score(geq, postings.get(i).getIdf(), TFIDFOrBM25);
                         }
-                    } else if (postings.get(i).getPostingActual().getDoc_id()>current&&conjunctive) {
-                        skip=true;
+                    } else if (postings.get(i).getPostingActual().getDoc_id() > current && conjunctive) {
+                        skip = true;
                         break;
-                    } else if (postings.get(i).getPostingActual().getDoc_id()==current) {
-                        score+=Scorer.score(postings.get(i).getPostingActual(),postings.get(i).getIdf(),TFIDFOrBM25);
+                    } else if (postings.get(i).getPostingActual().getDoc_id() == current) {
+                        score += Scorer.score(postings.get(i).getPostingActual(), postings.get(i).getIdf(), TFIDFOrBM25);
                     }
-                } else if (postings.get(i).getPostingActual()==null&&conjunctive) {
+                } else if (postings.get(i).getPostingActual() == null && conjunctive) {
                     return topKPriorityQueue;
                 }
             }
-            if(skip&&conjunctive){
+
+            if (skip && conjunctive) {
                 continue;
             }
-            if(topKPriorityQueue.offer(new Pair<>(score,current))){
-                threshold=topKPriorityQueue.peek().getKey();
-                while (pivot<postings.size()&&ub[pivot]<threshold){
+
+            if (topKPriorityQueue.offer(new Pair<>(score, current))) {
+                threshold = topKPriorityQueue.peek().getKey();
+                while (pivot < postings.size() && ub[pivot] < threshold) {
                     pivot++;
                 }
             }
         }
+
         return topKPriorityQueue;
     }
-    private static boolean isFinished(ArrayList<PostingIndex>postingIndices,int start,int end){
-        for(int i=start;i<end;i++){
-            if(postingIndices.get(i).getPostingActual()!=null){
+
+    /**
+     * Checks if the postings from start to end are finished.
+     *
+     * @param postingIndices The posting indices to check.
+     * @param start          The start index.
+     * @param end            The end index.
+     * @return True if the postings are finished, false otherwise.
+     */
+    private static boolean isFinished(ArrayList<PostingIndex> postingIndices, int start, int end) {
+        for (int i = start; i < end; i++) {
+            if (postingIndices.get(i).getPostingActual() != null) {
                 return false;
             }
         }
         return true;
     }
-    private static int getMinDocId(ArrayList<PostingIndex> postings,int start,int end) {
-        int min_doc = CollectionStatistics.getDocuments();
-        for (int i=start;i<end;i++) {
+
+    /**
+     * Gets the minimum document ID from postings.
+     *
+     * @param postings The list of posting indices.
+     * @param start    The start index.
+     * @param end      The end index.
+     * @return The minimum document ID.
+     */
+    private static int getMinDocId(ArrayList<PostingIndex> postings, int start, int end) {
+        int minDoc = CollectionStatistics.getDocuments();
+
+        for (int i = start; i < end; i++) {
             if (postings.get(i).getPostingActual() != null) {
-                min_doc = Math.min(min_doc, postings.get(i).getPostingActual().getDoc_id());
+                minDoc = Math.min(minDoc, postings.get(i).getPostingActual().getDoc_id());
             }
         }
-        return min_doc;
+
+        return minDoc;
     }
-    private static int get_doc_id(ArrayList<PostingIndex>postings,int start,int end){
-        int doc_id=get_max_doc_id(postings,start,end);
-        if(doc_id==0){
+
+    /**
+     * Gets the document ID using the max score strategy.
+     *
+     * @param postings The list of posting indices.
+     * @param start    The start index.
+     * @param end      The end index.
+     * @return The document ID.
+     */
+    private static int get_doc_id(ArrayList<PostingIndex> postings, int start, int end) {
+        int doc_id = get_max_doc_id(postings, start, end);
+
+        if (doc_id == 0) {
             return 0;
         }
-        for(int i=start;i<end;i++){
-            if(areEquals(postings, start, end)){
+
+        for (int i = start; i < end; i++) {
+            if (areEquals(postings, start, end)) {
                 return doc_id;
             }
-            if(postings.get(i).getPostingActual()==null){
+
+            if (postings.get(i).getPostingActual() == null) {
                 return 0;
             }
+
             if (postings.get(i).getPostingActual().getDoc_id() > doc_id) {
                 doc_id = postings.get(i).getPostingActual().getDoc_id();
-                i = start-1; // Reset i to restart the loop.
+                i = start - 1; // Reset i to restart the loop.
                 continue;
             }
+
             if (postings.get(i).getPostingActual().getDoc_id() < doc_id) {
                 Posting geq = postings.get(i).nextGEQ(doc_id);
+
                 if (geq == null) {
                     return 0;
                 }
+
                 if (geq.getDoc_id() > doc_id) {
                     doc_id = geq.getDoc_id();
-                    i = start-1; // Reset i to restart the loop.
+                    i = start - 1; // Reset i to restart the loop.
                     continue;
                 }
-                if(geq.getDoc_id()==doc_id){
-                    if(areEquals(postings,start,end)){
+
+                if (geq.getDoc_id() == doc_id) {
+                    if (areEquals(postings, start, end)) {
                         return doc_id;
                     }
-                    i=start-1;
-
-
+                    i = start - 1;
                 }
             }
         }
         return 0;
     }
-    private static boolean areEquals(ArrayList<PostingIndex>postings,int start,int end){
-        if(start+1==end||start==end){
+
+    /**
+     * Checks if the postings are equal from start to end.
+     *
+     * @param postings The list of posting indices.
+     * @param start    The start index.
+     * @param end      The end index.
+     * @return True if the postings are equal, false otherwise.
+     */
+    private static boolean areEquals(ArrayList<PostingIndex> postings, int start, int end) {
+        if (start + 1 == end || start == end) {
             return true;
         }
-        if(postings.get(0).getPostingActual()==null){
+
+        if (postings.get(0).getPostingActual() == null) {
             return false;
         }
-        int doc_id=postings.get(0).getPostingActual().getDoc_id();
-        for(int i=start+1;i<end;i++){
-            if(postings.get(i).getPostingActual()!=null){
-                if(postings.get(i).getPostingActual().getDoc_id()!=doc_id){
+
+        int doc_id = postings.get(0).getPostingActual().getDoc_id();
+
+        for (int i = start + 1; i < end; i++) {
+            if (postings.get(i).getPostingActual() != null) {
+                if (postings.get(i).getPostingActual().getDoc_id() != doc_id) {
                     return false;
                 }
-            }else{
+            } else {
                 return false;
             }
         }
+
         return true;
     }
-    private static int get_max_doc_id(ArrayList<PostingIndex>postings,int start,int end){
-        int doc_id=0;
-        for(int i=start;i<end;i++){
-            if(postings.get(i).getPostingActual()!=null){
+
+    /**
+     * Gets the maximum document ID from postings.
+     *
+     * @param postings The list of posting indices.
+     * @param start    The start index.
+     * @param end      The end index.
+     * @return The maximum document ID.
+     */
+    private static int get_max_doc_id(ArrayList<PostingIndex> postings, int start, int end) {
+        int doc_id = 0;
+
+        for (int i = start; i < end; i++) {
+            if (postings.get(i).getPostingActual() != null) {
                 if (postings.get(i).getPostingActual().getDoc_id() > doc_id) {
                     doc_id = postings.get(i).getPostingActual().getDoc_id();
                 }
-            }else{
+            } else {
                 return 0;
             }
         }
+
         return doc_id;
     }
-
 }
