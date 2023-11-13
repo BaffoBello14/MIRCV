9d7bb1a4ec82b3dac83b732ca70249ea1c791468
fratifederico <121012872+fratifederico@users.noreply.github.com>
2023-11-13 15:16:21 +0100

    added query test
    added query test

---
+++ src/test/java/it/unipi/QueryTest.java
@@ -0,0 +1,94 @@
+package it.unipi;
+import it.unipi.MIRCV.Query.Processer;
+import it.unipi.MIRCV.Utils.PathAndFlags.PathAndFlags;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+
+public class QueryTest {
+
+    static boolean conjunctive = false;
+    static String scoringFunction = "tfidf";
+
+    private static void createDirectoryIfNotExists(String directoryPath) {
+        try {
+            if (!Files.exists(Paths.get(directoryPath))) {
+                Files.createDirectories(Paths.get(directoryPath));
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    static void initialize() {
+        String[] directories = {
+                "./IndexDataTest/BlockInfo/",
+                "./IndexDataTest/CollectionStatistics/",
+                "./IndexDataTest/Doc_ids/",
+                "./IndexDataTest/Doc_index/",
+                "./IndexDataTest/Final/",
+                "./IndexDataTest/Flags/",
+                "./IndexDataTest/Freqs/",
+                "./IndexDataTest/Lexicons/"
+        };
+
+        for (String directory : directories) {
+            createDirectoryIfNotExists(directory);
+        }
+
+        PathAndFlags.PATH_TO_DOC_ID = "./IndexDataTest/Doc_ids/";
+        PathAndFlags.PATH_TO_FINAL = "./IndexDataTest/Final";
+        PathAndFlags.PATH_TO_FREQ = "./IndexDataTest/Freqs/";
+        PathAndFlags.PATH_TO_LEXICON = "./IndexDataTest/Lexicons/";
+        PathAndFlags.PATH_TO_FINAL_DOC_ID = "./IndexDataTest/Final/DocID.dat";
+        PathAndFlags.PATH_TO_FINAL_FREQ = "./IndexDataTest/Final/Freq.dat";
+        PathAndFlags.PATH_TO_DOC_INDEX = "./IndexDataTest/Doc_index/DocIndex.dat";
+        PathAndFlags.PATH_TO_FINAL_LEXICON = "./IndexDataTest/Final/Lexicon.dat";
+        PathAndFlags.PATH_TO_COLLECTION_STAT = "./IndexDataTest/CollectionStatistics/CollectionStat.dat";
+        PathAndFlags.PATH_TO_BLOCK_FILE = "./IndexDataTest/BlockInfo/BlockInfo.dat";
+        PathAndFlags.PATH_TO_FLAGS = "./IndexDataTest/Flags/Flags.dat";
+    }
+
+    public static void testQuery() {
+        ArrayList<String> queryList = new ArrayList<>();
+        queryList.add("Python programming language");
+        queryList.add("passion for coding");
+        queryList.add("quick brown fox lazy dog");
+        queryList.add("versatile programming");
+        queryList.add("enjoy reading books");
+
+        for (String query : queryList) {
+            testQueryWithOptions(query);
+        }
+    }
+
+    private static void testQueryWithOptions(String query) {
+        ArrayList<Integer> result;
+
+        for (int i = 0; i < 2; i++) {
+            for (int j = 0; j < 2; j++) {
+                for (int k = 0; k < 2; k++) {
+                    conjunctive = (i == 1);
+                    scoringFunction = (j == 1) ? "bm25" : "tfidf";
+                    PathAndFlags.DYNAMIC_PRUNING = (k == 1);
+
+                    System.out.println(Processer.processQuery(query, 3, conjunctive, scoringFunction));
+
+                    //printQueryResult(query, conjunctive, scoringFunction, result);
+                }
+            }
+        }
+    }
+
+    private static void printQueryResult(String query, boolean conjunctive, String scoringFunction, ArrayList<Integer> result) {
+        System.out.println("Query: " + query + "\nParameters:\tconjunctive:" + conjunctive + "\tScoring function: " + scoringFunction);
+        System.out.println("Result: " + result);
+    }
+
+    public static void main(String[] args) {
+        QueryTest.initialize(); // Inizializzazione dei percorsi
+        QueryTest.testQuery(); // Esecuzione dei test
+    }
+
+}
