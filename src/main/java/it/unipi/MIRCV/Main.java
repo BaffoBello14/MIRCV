f30cd9b7407d90b95084786bac7944a9f7400b6a
Giulio Bello <giuliobello12@gmail.com>
2023-11-14 17:58:25 +0100

    Refactor code for better readability and user input handling, start of report

--- src/main/java/it/unipi/MIRCV/Main.java
+++ src/main/java/it/unipi/MIRCV/Main.java
@@ -8,58 +8,57 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args){
+    public static void main(String[] args) {
         CollectionStatistics.readFromDisk();
         PathAndFlags.readFlagsFromDisk();
         Lexicon.getInstance();
         DocIndex.getInstance();
-        LexiconEntry entry=new LexiconEntry();
-        SkippingBlock skippingBlock=new SkippingBlock();
-        Scanner scanner= new Scanner(System.in);
-        long timerStart,timerEnd;
-        ArrayList<Integer>queryResult;
-        String query,scoreFun;
+        LexiconEntry entry = new LexiconEntry();
+        SkippingBlock skippingBlock = new SkippingBlock();
+        Scanner scanner = new Scanner(System.in);
+        long timerStart, timerEnd;
+        ArrayList<Integer> queryResult;
+        String query, scoreFun;
         do {
             System.out.print("Query-> ");
-            query=scanner.nextLine();
-            if(query.trim().isEmpty()){
+            query = scanner.nextLine();
+            if (query.trim().isEmpty()) {
                 System.out.println("empty query");
                 continue;
             }
             System.out.print("Daat(1) or Dynamic Pruning(2) or exit(3)?");
-            int chose=Integer.parseInt(scanner.nextLine());
-            if(chose!=1&&chose!=2&&chose!=3){
+            int chose = Integer.parseInt(scanner.nextLine());
+            if (chose != 1 && chose != 2 && chose != 3) {
                 System.out.println("no good chose then repeat");
                 continue;
-            } else if (chose==3) {
+            } else if (chose == 3) {
                 break;
             }
             System.out.print("Conjunctive(1) or Disjunctive(2)?");
-            int chose1=Integer.parseInt(scanner.nextLine());
-            if(chose1!=1&&chose1!=2){
+            int chose1 = Integer.parseInt(scanner.nextLine());
+            if (chose1 != 1 && chose1 != 2) {
                 System.out.println("no conjunctive nor disjunctive selected so restart");
                 continue;
             }
             System.out.print("score fun bm25 or tfidf->");
-            scoreFun=scanner.nextLine();
-            if(!scoreFun.equals("bm25")&&!scoreFun.equals("tfidf")){
+            scoreFun = scanner.nextLine();
+            if (!scoreFun.equals("bm25") && !scoreFun.equals("tfidf")) {
                 System.out.println("no tfidf or bm25");
                 continue;
             }
-            PathAndFlags.DYNAMIC_PRUNING= chose != 1;
-            timerStart=System.currentTimeMillis();
-            queryResult=Processer.processQuery(query,10,chose1==1,scoreFun);
-            timerEnd=System.currentTimeMillis();
-            if(queryResult==null){
+            PathAndFlags.DYNAMIC_PRUNING = chose != 1;
+            timerStart = System.currentTimeMillis();
+            queryResult = Processer.processQuery(query, 10, chose1 == 1, scoreFun);
+            timerEnd = System.currentTimeMillis();
+            if (queryResult == null) {
                 System.out.println("no docs for the query");
-            }else{
+            } else {
                 System.out.print("Results of DocNos-> ");
-                for(int i:queryResult){
-                    System.out.print(DocIndex.getInstance().getDoc_NO(i)+" ");
+                for (int i : queryResult) {
+                    System.out.print(DocIndex.getInstance().getDoc_NO(i) + " ");
                 }
-                System.out.println("with time->"+(timerEnd-timerStart)+"ms");
+                System.out.println("with time->" + (timerEnd - timerStart) + "ms");
             }
-        }while(true);
+        } while (true);
     }
 }
-
